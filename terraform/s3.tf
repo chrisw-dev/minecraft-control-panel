e99ca7a087dc77f009e429658330af101a35f80f
chrisw-dev <68233262+chrisw-dev@users.noreply.github.com>
2024-01-02 10:08:27 +0000

    initial terraform work

---
+++ terraform/s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "bucket" {
+    bucket = "my-bucket-name"  // replace with a unique bucket name
+    acl    = "public-read"
+
+    website {
+        index_document = "index.html"
+        error_document = "error.html"
+    }
+}
