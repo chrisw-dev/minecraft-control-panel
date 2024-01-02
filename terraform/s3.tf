7bdd489a8353374c9342d8352f39934cc5599e4e
chrisw-dev <68233262+chrisw-dev@users.noreply.github.com>
2024-01-02 10:12:23 +0000

    Refactor CloudFront and S3 configurations

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,9 @@
 resource "aws_s3_bucket" "bucket" {
-    bucket = "my-bucket-name"  // replace with a unique bucket name
-    acl    = "public-read"
+  bucket = "my-bucket-name" // replace with a unique bucket name
+  acl    = "public-read"
 
-    website {
-        index_document = "index.html"
-        error_document = "error.html"
-    }
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
